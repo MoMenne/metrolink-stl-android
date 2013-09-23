842741bffd0cc710ecd4417f95d38e9c7d38a18e
MoMenne <mpmenne@electronicsystemssupport.com>
2013-09-22 21:27:27 -0500

    refactoring and fixed the time format bug for showing hours

---
+++ src/com/example/metrolink_stl_android/Util.java
@@ -0,0 +1,11 @@
+package com.example.metrolink_stl_android;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mpmenne
+ * Date: 9/22/13
+ * Time: 5:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Util {
+}
